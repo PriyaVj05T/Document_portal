e0c83aced86572a24d426f92d384d3e7e3e63145
PriyaVj05T <vijayvargiyapriya05@gmail.com>
2025-07-27 17:59:14 +0530

    set diff. llm provider as default- token error

--- model/models.py
+++ model/models.py
@@ -1,3 +1,15 @@
-#we use pydantric for data validation and settings management.
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any, Union
+
+
+class Metadata(BaseModel):
+    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Title: str
+    Author: str
+    DateCreated: str   
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]  # Can be "Not Available"
+    SentimentTone: str
+    
